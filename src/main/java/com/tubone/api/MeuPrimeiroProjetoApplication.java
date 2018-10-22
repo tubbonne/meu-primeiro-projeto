1428d4e2e092724586edbf0817f5de2b5a454943
Alex Tubone <tubbonne@gmail.com>
2018-10-22 16:26:20 -0300

    Test

--- src/main/java/com/tubone/api/MeuPrimeiroProjetoApplication.java
+++ src/main/java/com/tubone/api/MeuPrimeiroProjetoApplication.java
@@ -8,6 +8,6 @@ public class MeuPrimeiroProjetoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MeuPrimeiroProjetoApplication.class, args);
-		System.out.println("Meu primeiro projeto...");
+		System.out.println("Meu primeiro projeto......");
 	}
 }
